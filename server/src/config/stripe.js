c813c962cef1b2908615842bc46c9c1377815c2e
ChinmayBisoy <chinmaybisoyi079@gmail.com>
2026-04-03 02:38:55 +0530

    Initial commit: Tech_Mates MERN project

--- server/src/config/stripe.js
+++ server/src/config/stripe.js
@@ -1,14 +1,3 @@
-
-const Stripe = require('stripe');
-
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-
-if (!stripeSecretKey) {
-  console.warn('STRIPE_SECRET_KEY is not set. Stripe calls will fail until it is configured.');
-}
-
-const stripe = new Stripe(stripeSecretKey || 'sk_test_000000000000000000000000', {
-  apiVersion: '2024-06-20',
-});
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = stripe;
